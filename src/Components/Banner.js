83fb4c96ed1ab8adfb7d2d61599990d0c923960e
Devilbat75 <112400858+Devilbat75@users.noreply.github.com>
2023-04-07 18:04:32 +0200

    Début de code

--- src/Components/Banner.js
+++ src/Components/Banner.js
@@ -1,8 +1,11 @@
-import '../Styles/Banner.css'
+import "../styles/Banner.css";
+import logo from "../Assets/.LOGO-1.png";
 
-// composent children pour le cas ou il y aurait plus de 2 bannières.
-function Banner({children}) { 
-    return (<div>{children}</div>)
+function Banner() {
+    return( <div className="kasa-banner">
+                <img src={logo} alt="Logo Kasa" className="kasa-logo"/>
+                <h1 className="kasa-title">Kasa</h1>
+            </div>)
 }
 
 export default Banner
