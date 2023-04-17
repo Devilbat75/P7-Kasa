99709e35e84bf49e6fb9654a2c2937146a2f40f7
Devilbat75 <112400858+Devilbat75@users.noreply.github.com>
2023-04-17 17:08:13 +0200

    Fin route

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,8 @@ import { Route, BrowserRouter as Router, Routes } from 'react-router-dom'
 
 import Home from './pages/Home';
 import About from './pages/About';
+import Logement from './pages/Logement';
+import Error from './pages/Error';
 
 function App() {
     return (
@@ -12,6 +14,8 @@ function App() {
             <Routes>
                 <Route exact path="/" element={<Home />} />
                 <Route exact path="/about" element={<About />} />
+                <Route exact path='/logement/:id' element={<Logement />} />
+                <Route exact path='*' element={<Error />} />
             </Routes>
         </Router>)
 }
