5cc0262b62015525283d6071f27db881e0876fc2
Devilbat75 <112400858+Devilbat75@users.noreply.github.com>
2023-04-24 17:13:10 +0200

    Maj erreur

--- src/pages/Error.jsx
+++ src/pages/Error.jsx
@@ -1,11 +1,9 @@
 import '../styles/Error.css';
-import Header from '../components/Header';
 import { Link } from 'react-router-dom';
 
 function Error() {
 	return (
 		<div className='Error'>
-			<Header />
 			<div className="Error_infos">
 				<h1 className='Error_infos_title'>404</h1>
 				<p className='Error_infos_content'>La page que vous recherchez n'existe pas.</p>
