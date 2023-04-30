59134ebdf110f3e2b4e6c1b4160eb03e797d8143
Devilbat75 <112400858+Devilbat75@users.noreply.github.com>
2023-05-01 01:09:50 +0200

    logement fini

--- src/pages/Logement.jsx
+++ src/pages/Logement.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
+import '../styles/Logement.css'
 import items from '../datas/item.json'
 import Collapse from '../components/Collapse'
 import Carousel from '../components/Carousel'
 import { useParams } from 'react-router-dom'
 import { useEffect, useState } from 'react'
+import etoileGrise from '../assets/étoiles_grises.png'
+import etoileRouge from '../assets/étoiles_rouges.png'
 
 function Logement() {
 	
@@ -20,13 +23,14 @@ function Logement() {
 	const name = itemCurrentLogement[0].host.name.split(' '); 
 	const description  = itemCurrentLogement[0].description;
 	const equipments = itemCurrentLogement[0].equipments;
+	const rating = itemCurrentLogement[0].rating;
 
 	return (
 		<>
 			<Carousel imageSlider={imageSlider}/>
-			<main className="accomodation">
-				<div className="accomodation_content">
-					<div className="accomodation_content_infos">
+			<main className="logement">
+				<div className="logement_content">
+					<div className="logement_content_infos">
 						<h1>{itemCurrentLogement[0].title}</h1>
 						<p>{itemCurrentLogement[0].location}</p>
 						<div>
@@ -37,21 +41,29 @@ function Logement() {
 							})}
 						</div>
 					</div>
-					<div className="accomodation_content_host">
+					<div className="logement_content_host">
 						<div>
-							<div className='accomodation_content_host_name'>
+							<div className='logement_content_host_name'>
 								<span>{name[0]}</span>
 								<span>{name[1]}</span>
 							</div>
-							<img src={itemCurrentLogement[0].host.picture} alt="host of this accomodation" />
+							<img src={itemCurrentLogement[0].host.picture} alt="propriétaire de ce logement" />
+						</div>
+						<div className="logement_content_host_stars">
+							{[...Array(5)].map((star, index) => {
+								const ratingValue = index + 1;
+								return (
+									<img key={index} src={ratingValue <= rating ? etoileRouge : etoileGrise} alt="star" />
+								)
+							})}
 						</div>
 					</div>
 				</div>
-				<div className="accomodation_collapse">
-					<div className="accomodation_collapse_item">
+				<div className="logement_collapse">
+					<div className="logement_collapse_item">
 						<Collapse title={'Description'} content={description} />	
 					</div>
-					<div className="accomodation_collapse_item">
+					<div className="logement_collapse_item">
 						<Collapse title={'Équipements'} content={equipments}/>
 					</div>	
 				</div>
