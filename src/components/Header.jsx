6071ce960414ff3e16d1eb3210b22f5fd648fb8b
Devilbat75 <112400858+Devilbat75@users.noreply.github.com>
2023-05-01 18:01:46 +0200

    finition

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,17 +1,26 @@
 import React from 'react'
 import "../styles/Header.css";
 import logo from "../assets/.LOGO-1.png";
-import { Link } from 'react-router-dom';
+import { Link, useLocation  } from 'react-router-dom';
 
 function Header() {
+
+    const location = useLocation();
+    const currentUrl = location.pathname;
+    let classCssHome="kasa-accueil";
+    let classCssAbout="kasa-a-propos";
+
+    if(currentUrl === "/") classCssHome="nav-active kasa-accueil";
+    if(currentUrl === "/about") classCssAbout="nav-active kasa-a-propos";
+    
     return (<header className="kasa-header">
         <div>
             <img src={logo} alt="Logo Kasa" />
         </div>
         <nav className='kasa-nav'>
             <ul className='kasa-nav-list'>
-                <Link to="/"><li className="kasa-accueil">Accueil</li></Link>
-                <Link to="/about"><li className="kasa-a-propos">A Propos</li></Link>
+                <Link to="/"><li  className={classCssHome}>Accueil</li></Link>
+                <Link to="/about"><li className={classCssAbout}>A Propos</li></Link>
             </ul>
         </nav>
     </header>)
